040a70bf4d23898d56b12f57a27da18589c0b462
Javier Gonzalez <javiergonzalez@macbook-pro-de-xav.home>
2021-02-20 11:04:28 +0100

    ajout admin

--- inc/inc.config.php
+++ inc/inc.config.php
@@ -1,57 +1,36 @@
 <?php
 
-	// ---- Définition des constantes du site ------------------------ //
-	//echo $_SERVER[ "DOCUMENT_ROOT" ] . "<br>";
-	switch( $_SERVER[ "DOCUMENT_ROOT" ] ) {
-		
-		// ---- Serveur local Franck -------- //
-		case "/var/www/edenblue" :
-			$localhost = "localhost";
-			$dbname = "edenblue";
-			$user = "edenblue";
-			$mdp = "edenblue33";
-			break;
-		
-		// ---- Serveur PRE-PROD ------------ //
-		case "" :
-			$localhost = "";
-			$dbname = "";
-			$user = "";
-			$mdp = "";
-			break;
-		
-		// ---- Site en PROD ------------------------ //
-		case "/var/www/edenblue.fr" :
-			$localhost = "localhost";
-			$dbname = "edenblue";
-			$user = "edenblue";
-			$mdp = "edenblue33";
-			break;
-			
-		default:
-			$localhost = "localhost";
-			$dbname = "edenblue";
-			$user = "edenblue";
-			$mdp = "edenblue33";
-			break;
-	}
-	
-	define( "DBHOST", $localhost );
-	define( "DBNAME", $dbname );
-	define( "DBUSER", $user );
-	define( "DBPASSWD", $mdp );
-	
-	define( "TVA", 				0.2 );
-	define( "FRAIS_LIVRAISON", 	5 ); 		// Frais de livraison TTC
-	define( "SEUIL_FDL_GRATOS", 41.666 );	// Frais de port gratuits au delà de 50€ TTC
-	
-	define( "MAIL_TEST", 			"" );				// Si vide alors les mails partent vers les bons destinataires!!!
-	define( "MAIL_CUSTOMER", 		"contact@edenblue.fr" );
-	define( "MAIL_NAME_CUSTOMER", 	"edenblue" );
-	define( "URL_SITE_DEFAULT", 	"http://www.edenblue.fr/" );
-	define( "FACEBOOK_LINK", 		"https://www.facebook.com/edenblue" );
-	
-	// Mail d'envoi
-	define( "MAIL_CONTACT", "contact@edenblue.fr" );
-	define( "MAIL_BCC", 	"xav335@hotmail.com,fjavi.gonzalez@gmail.com,jav_gonz@yahoo.com" );
-?>
+if ($_SERVER[ "SERVER_NAME" ] == "edenblue.localxav.lan") {
+    define("DBHOST", "localhost");
+    define("DBNAME", "edenblue");
+    define("DBUSER", "edenblue");
+    define("DBPASSWD", "edenblue33");
+} elseif ($_SERVER[ "SERVER_NAME" ] == "dev.edenblue.fr") {
+    define("DBHOST", "edenbluesy1.mysql.db");
+    define("DBNAME", "edenbluesy1");
+    define("DBUSER", "edenbluesy1");
+    define("DBPASSWD", "Edenblue33");
+} else {
+    define("DBHOST", "edenbluesyprod.mysql.db");
+    define("DBNAME", "edenbluesyprod");
+    define("DBUSER", "edenbluesyprod");
+    define("DBPASSWD", "Edenblue33");
+}
+
+
+
+// CONSTANTES CONSTANTES CONSTANTES CONSTANTES CONSTANTES CONSTANTES CONSTANTES CONSTANTES
+$tva = 0.2;
+// Frais de livraison par defaut
+$totalLiv = 10; //frais de livraison TTC
+
+$mailCustomer = "contact@edenblue.fr";
+$mailNameCustomer = "EdenBlue";
+$urlSiteDefault = "http://www.edenblue.fr/";
+$facebookLink = "https://www.facebook.com/edenbluepiscine";
+
+//mail d'envoi
+//$mailContact = "fjavi.gonzalez@gmail.com";
+$mailContact = "contact@edenblue.fr";
+$mailBcc = "fjavi.gonzalez@gmail.com,xav335@hotmail.com,xavier.gonzalez@laposte.net,jav_gonz@yahoo.com";
+//$mailBcc = "edenblue33@gmail.com,fjavi.gonzalez@gmail.com";
