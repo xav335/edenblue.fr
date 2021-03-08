c66ca873f2e87999614129127c5b950ef1bbc900
Javier <jgonzalez@iconeo.frm>
2021-03-08 17:39:21 +0100

    spa serie club done

--- spa-serie-club.php
+++ spa-serie-club.php
@@ -10,9 +10,11 @@
 
   <main class="grid-container">
 
-    <section class="grid-x grid-padding-x" role="">
+    <section class="grid-x grid-padding-x" role="spadetail">
       <div class="large-12 medium-12 small-12 cell">
-       <h1 data-animation="top">Serie Club</h1>
+        <h1 data-animation="top">Serie Club</h1>
+      </div>
+      <div class="large-7 medium-12 small-12 cell">
         <h3>La detente a prix abordable</h3>
         <p data-animation="top">
             les spas de la série Club entrent dans la catégorie des spas familiaux.  </br>
@@ -20,41 +22,89 @@
             La série Club est propulsée par La l’in.ye de Gecko, le système de spa le plus avancé , le plus robuste, le plus flexible et le plus sécuritaire.
         </p>
       </div>
-      <div class="large-3 medium-3 small-12 cell">
-        <a href="">
+       <div class="large-5 medium-12 small-12 cell">
+        <a data-fancybox="spa1" href="assets/img/gammeclub/cl84c.png">
+            <img src="assets/img/gammeclub/cl84c.png" alt="Spa Club Edenblue">
+        </a>    
+      </div>
+      </br>
+      <div class="large-3 medium-4 small-12 cell">
+        <a data-fancybox="spa" href="assets/img/gammeclub/cl84.png">
             <figcaption>SERIE CLUB - CLB4</figcaption>
-            <img src="assets/img/gammeclub/cl84.png" alt="Nos réalisations contemporaines">
-        </a>
+            <img src="assets/img/gammeclub/cl84.png" alt="Spa Club Edenblue">
+        </a>    
+        <p>
+            <strong>Dimensions</strong> : 183 x183 x 86 cm </br>
+            <strong>Nombre de places</strong> : 3 dont 1 allongé</br>
+            <strong>Nombre de pompes</strong> : 1 bi-vitesses (3 CV)</br>
+            <strong>Buses</strong> : 26 </br>
+            <strong>Equipements</strong> : UV/ozone, projecteur leds, fontaines éclairées, couverture </br>
+        </p>
       </div>
-      <div class="large-3 medium-3 small-12 cell">
-        <a href="">
+      <div class="large-3 medium-4 small-12 cell">
+         <a data-fancybox="spa" href="assets/img/gammeclub/cl85.png">
             <figcaption>SERIE CLUB - CLB5</figcaption>
-            <img src="assets/img/gammeclub/cl85.png" alt="Nos réalisations contemporaines">
+            <img src="assets/img/gammeclub/cl85.png" alt="Spa Club Edenblue">
         </a>
+        <p>
+            <strong>Dimensions</strong> : 183 x183 x 86 cm </br>
+            <strong>Nombre de places</strong> : 3 dont 1 allongé</br>
+            <strong>Nombre de pompes</strong> : 1 bi-vitesses (3 CV)</br>
+            <strong>Buses</strong> : 26 </br>
+            <strong>Equipements</strong> : UV/ozone, projecteur leds, fontaines éclairées, couverture </br>
+        </p>
       </div>
       <div class="large-3 medium-3 small-12 cell">
-        <a href="">
+        <a data-fancybox="spa" href="assets/img/gammeclub/cl86.png">
             <figcaption>SERIE CLUB - CLB6</figcaption>
-            <img src="assets/img/gammeclub/cl86.png" alt="Nos réalisations contemporaines">
+            <img src="assets/img/gammeclub/cl86.png" alt="Spa Club Edenblue">
         </a>
+        <p>
+            <strong>Dimensions</strong> : 183 x183 x 86 cm </br>
+            <strong>Nombre de places</strong> : 3 dont 1 allongé</br>
+            <strong>Nombre de pompes</strong> : 1 bi-vitesses (3 CV)</br>
+            <strong>Buses</strong> : 26 </br>
+            <strong>Equipements</strong> : UV/ozone, projecteur leds, fontaines éclairées, couverture </br>
+        </p>
       </div>
-       <div class="large-3 medium-3 small-12 cell">
-        <a href="">
+       <div class="large-3 medium-4 small-12 cell">
+         <a data-fancybox="spa" href="assets/img/gammeclub/cl87.png">
             <figcaption>SERIE CLUB - CLB7</figcaption>
-            <img src="assets/img/gammeclub/cl87.png" alt="Nos réalisations contemporaines">
+            <img src="assets/img/gammeclub/cl87.png" alt="Spa Club Edenblue">
         </a>
+        <p>
+            <strong>Dimensions</strong> : 183 x183 x 86 cm </br>
+            <strong>Nombre de places</strong> : 3 dont 1 allongé</br>
+            <strong>Nombre de pompes</strong> : 1 bi-vitesses (3 CV)</br>
+            <strong>Buses</strong> : 26 </br>
+            <strong>Equipements</strong> : UV/ozone, projecteur leds, fontaines éclairées, couverture </br>
+        </p>
       </div>
-       <div class="large-3 medium-3 small-12 cell">
-        <a href="">
+       <div class="large-3 medium-4 small-12 cell">
+        <a data-fancybox="spa" href="assets/img/gammeclub/cl86.png">
             <figcaption>SERIE CLUB - CLB8</figcaption>
-            <img src="assets/img/gammeclub/cl86.png" alt="Nos réalisations contemporaines">
+            <img src="assets/img/gammeclub/cl86.png" alt="Spa Club Edenblue">
         </a>
+        <p>
+            <strong>Dimensions</strong> : 183 x183 x 86 cm </br>
+            <strong>Nombre de places</strong> : 3 dont 1 allongé</br>
+            <strong>Nombre de pompes</strong> : 1 bi-vitesses (3 CV)</br>
+            <strong>Buses</strong> : 26 </br>
+            <strong>Equipements</strong> : UV/ozone, projecteur leds, fontaines éclairées, couverture </br>
+        </p>
       </div>
-       <div class="large-3 medium-3 small-12 cell">
-        <a href="">
+       <div class="large-3 medium-4 small-12 cell">
+         <a data-fancybox="spa" href="assets/img/gammeclub/clb9sf.png">
             <figcaption>SERIE CLUB - CLB9ST</figcaption>
-            <img src="assets/img/gammeclub/clb9sf.png" alt="Nos réalisations contemporaines">
+            <img src="assets/img/gammeclub/clb9sf.png" alt="Spa Club Edenblue">
         </a>
+        <p>
+            <strong>Dimensions</strong> : 183 x183 x 86 cm </br>
+            <strong>Nombre de places</strong> : 3 dont 1 allongé</br>
+            <strong>Nombre de pompes</strong> : 1 bi-vitesses (3 CV)</br>
+            <strong>Buses</strong> : 26 </br>
+            <strong>Equipements</strong> : UV/ozone, projecteur leds, fontaines éclairées, couverture </br>
+        </p>
       </div>
        
     </section>
