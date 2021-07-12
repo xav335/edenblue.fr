3bf6de1a982309b34ca7b0434ec8f65ad1735f40
javier gonzalez <jgonzalez@iconeo.fr>
2021-07-12 17:12:06 +0200

    404 avec header

--- 404.php
+++ 404.php
@@ -1,34 +1,3 @@
 <?php 
-include_once 'inc/inc.config.php';
-include_once 'admin/classes/utils.php';
-?>
-<!doctype html>
-<html class="no-js" lang="fr">
-  <head>
-    <?php include 'inc/inc.head.php'; ?>
-    <title>Eden Blue -  Techniques de construction</title>
-  </head>
-<body class="not-home">
-
-  <?php include 'inc/inc.header.php'; ?>
-
-  <main class="grid-container">
-
-    <div class="grid-x grid-padding-x" role="galery">
-      <div class="large-12 medium-12 small-12 cell" text-center>
-        <h1>Mentions Légales</h1>
-      </div>
-      
-       <div class="large-12 medium-12 small-12 cell" text-center>
-       <h2>Cette page n'existe pas !</h2>
-      </div>
-      
-      
-      
-    </div>   
-  </main>
-
-  <?php include 'inc/inc.footer.php'; ?>
-
-</body>
-</html>
+//print_r($_SERVER);
+header('Location: '. $_SERVER['REQUEST_SCHEME'].'://'.  $_SERVER['HTTP_HOST'] .'/index.php');
