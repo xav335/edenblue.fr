1c0e614aa02f6c1b6ede0d2cb8beaac4bc65710d
javier gonzalez <jgonzalez@iconeo.fr>
2021-07-12 13:24:04 +0200

    awstats

--- admin/home.php
+++ admin/home.php
@@ -32,7 +32,7 @@ if (empty($result)) {
 	<div class="row">
 		<div class="col-md-8">
 			<h3>Statistiques</h3>
-			<iframe id="laframe" src="http://www.iconeo.fr/awstats/awstats.pl?config=www.bsport.fr&framename=mainright" style="width:760px;height:700px;" frameborder="1" ></iframe>
+			<iframe id="laframe" src="http://www.iconeo.fr/awstats/awstats.pl?config=www.edenblue.fr&framename=mainright" style="width:760px;height:700px;" frameborder="1" ></iframe>
 		</div>
 		<div class="col-md-4">
 			<div class="panel panel-default">
