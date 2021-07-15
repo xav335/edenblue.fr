c1688f40f4c8728505fbe7e5c72f47db240befb8
root <root@vmprod.iconeo.fr>
2021-07-15 11:44:39 +0200

    analytics

--- inc/inc.footer.php
+++ inc/inc.footer.php
@@ -40,13 +40,12 @@
   <script src="assets/js/jquery.min.js"></script>
   <script src="assets/js/jquery.fancybox.min.js"></script>
   <script src="assets/js/app.js"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-VXBWZN26ST"></script>
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-  ga('create', 'UA-59487310-1', 'auto');
-  ga('send', 'pageview');
-
+    gtag('config', 'G-VXBWZN26ST');
 </script>
